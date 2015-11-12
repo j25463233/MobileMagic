14fa8a3f72c5bbeb64ddeea05c6828c84a2aefcc
Jason <jason0910@gmail.com>
2015-11-11 19:31:38 -0700

    Initial commit

---
+++ app/src/main/java/com/icarus/mobilemagic/MainActivity.java
@@ -0,0 +1,16 @@
+package com.icarus.mobilemagic;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String s = "Github is lame";
+    }
+}
