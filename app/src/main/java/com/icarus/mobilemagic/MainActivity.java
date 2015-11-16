05e4a0d44b23998a3f8428a061571fdf0a7f97ac
Jedediah <jedediah@me.com>
2015-11-15 21:06:03 -0700

    working on saving the chosen card to a file!

--- app/src/main/java/com/icarus/mobilemagic/MainActivity.java
+++ app/src/main/java/com/icarus/mobilemagic/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends FragmentActivity {
 
     @Override
@@ -19,4 +23,33 @@ public class MainActivity extends FragmentActivity {
             transaction.commit();
         }
     }
+
+
+    // Method used to save the selected card. This method will save a string of the suit and rank of the card
+    // and save it to a file on the internal storage.
+    public void saveSelectedCard(){
+
+        // will have to use toString();
+        String selectedCard = "";
+
+        // file name
+        String file_name = "Previously Selected Cards";
+
+        //attempt to open and save file
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(file_name, MODE_PRIVATE);
+            fileOutputStream.write(selectedCard.getBytes());
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+
+
+    }
 }
+
+
